f475016a30c992d3a640e357a9cb112d1cfa4f62
Partho <parthodas6176@gmail.com>
2023-10-09 23:27:51 +0530

    cleaup

--- LLM-VM/temp.py
+++ LLM-VM/temp.py
@@ -1,8 +1,13 @@
 from llm_vm.client import Client
+import os
+from dotenv import load_dotenv
 
+load_dotenv()  # Load the .env file
+
+open_ai_key = os.getenv("OPENAI_API_KEY")
 # Select which LLM you want to use, here we have OpenAI's 
 client = Client(big_model = 'chat_gpt')
 
 # Put in your prompt and go!
-response = client.complete(prompt = 'What is Anarchy?', context = '', openai_key = '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')
+response = client.complete(prompt = 'What is Anarchy?', context = '', openai_key = open_ai_key)
 print(response)

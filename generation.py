2435f22a44ed34538e40dcd6e4fa47abd762dad3
Partho <parthodas6176@gmail.com>
2023-10-10 01:14:24 +0530

    generate json dataset
    Co-authored-by: Karan Janthe <KMJ-007@users.noreply.github.com>

--- generation.py
+++ generation.py
@@ -1,16 +1,50 @@
- # import our client
 from llm_vm.client import Client
 import os
-import load_dotenv
+from dotenv import load_dotenv
+import json
 
 load_dotenv()  # Load the .env file
 
-# Select which LLM you want to use, here we have OpenAI's 
-client = Client(big_model = 'chat_gpt')
+open_ai_key = os.getenv("OPENAI_API_KEY")
 
-# Load your API key from an environment variable
-openai_api_key = os.getenv("OPENAI_API_KEY")
-# Put in your prompt and go!
-response = client.complete(prompt = 'how is the weather in delhi?', context = '', openai_key = openai_api_key)
-print(response)
-# Anarchy is a political ideology that advocates for the absence of government...
+generation_prompt = """Generate 40 examples of conversations, where an initial statement is made, followed by two replies, one that agrees with the initial statement and one that disagrees with the initial statement. The initial statement could be right, wrong, or neither right nor wrong.
+
+Structure of the conversation like this:
+[
+  {
+    "statement": "<initial statement>",
+    "agree": "<reply that agrees with the initial statement>",
+    "disagree": "<reply that disagrees with the initial statement>"
+  },
+  {
+    "statement": "<initial statement>",
+    "agree": "<reply that agrees with the initial statement>",
+    "disagree": "<reply that disagrees with the initial statement>"
+  }
+]"""
+
+
+def generate_dataset(model, n_call, filename="dataset.json"):
+    print("getting model...")
+    client = Client(big_model=model)
+
+    print("generating dataset...")
+    dataset = []
+
+    for i in range(n_call):
+        print(f"api call {i+1}/{n_call}")
+        response = client.complete(prompt=generation_prompt, openai_key=open_ai_key)
+        try:
+            data = json.loads(response["completion"])
+        except:
+            print("response skipped")
+            continue
+        dataset.extend(data)
+
+    print(f"writing dataset to {filename}...")
+    with open(filename, "w") as f:
+        json.dump(dataset, f, indent=2)
+
+
+if __name__ == "__main__":
+    generate_dataset(model="chat_gpt", n_call=3)
